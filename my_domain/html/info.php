27143fa0141b69ba92fb4dede9ea98aeea8b0985
Ubuntu <ubuntu@ip-172-31-45-196.eu-north-1.compute.internal>
2024-11-25 11:22:09 +0000

    Committer: Ubuntu <ubuntu@ip-172-31-45-196.eu-north-1.compute.internal>  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   my_domain/html/content.html 	new file:   my_domain/html/file.mp3 	new file:   my_domain/html/image1/cat.jpg 	new file:   my_domain/html/image2/cat.png 	new file:   my_domain/html/image_1.gif 	new file:   my_domain/html/index.html 	new file:   my_domain/html/index.nginx-debian.html 	new file:   my_domain/html/info.php 	new file:   nginx/fastcgi.conf 	new file:   nginx/fastcgi.conf.default 	new file:   nginx/fastcgi_params 	new file:   nginx/fastcgi_params.default 	new file:   nginx/koi-utf 	new file:   nginx/koi-win 	new file:   nginx/mime.types 	new file:   nginx/mime.types.default 	new file:   nginx/modules-available/50-mod-http-subs-filter.conf 	new file:   nginx/modules-enabled/50-mod-http-auth-pam.conf 	new file:   nginx/modules-enabled/50-mod-http-dav-ext.conf 	new file:   nginx/modules-enabled/50-mod-http-echo.conf 	new file:   nginx/modules-enabled/50-mod-http-geoip2.conf 	new file:   nginx/modules-enabled/50-mod-http-upstream-fair.conf 	new file:   nginx/modules-enabled/50-mod-stream.conf 	new file:   nginx/modules-enabled/70-mod-stream-geoip2.conf 	new file:   nginx/modules-enabled/ngx_http_image_filter_module.so 	new file:   nginx/modules-enabled/ngx_http_image_filter_module_modules.c 	new file:   nginx/modules-enabled/ngx_http_image_filter_module_modules.o 	new file:   nginx/nginx.conf 	new file:   nginx/nginx.conf.default 	new file:   nginx/proxy_params 	new file:   nginx/scgi_params 	new file:   nginx/scgi_params.default 	new file:   nginx/sites-available/default 	new file:   nginx/sites-available/my_domain 	new file:   nginx/sites-enabled/my_domain 	new file:   nginx/snippets/fastcgi-php.conf 	new file:   nginx/snippets/snakeoil.conf 	new file:   nginx/uwsgi_params 	new file:   nginx/uwsgi_params.default 	new file:   nginx/win-utf

---
+++ my_domain/html/info.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Информация о сервере</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<?php
+echo "<h1>Информация о сервере</h1>";
+
+echo "<h2>Server Details</h2>";
+echo "<p><strong>Server Name:</strong> " . $_SERVER['SERVER_NAME'] . "</p>";
+echo "<p><strong>Server IP:</strong> " . $_SERVER['SERVER_ADDR'] . "</p>";
+echo "<p><strong>Server Software:</strong> " . $_SERVER['SERVER_SOFTWARE'] . "</p>";
+echo "<p><strong>Server Protocol:</strong> " . $_SERVER['SERVER_PROTOCOL'] . "</p>";
+echo "<p><strong>Document Root:</strong> " . $_SERVER['DOCUMENT_ROOT'] . "</p>";
+echo "<p><strong>Request Method:</strong> " . $_SERVER['REQUEST_METHOD'] . "</p>";
+echo "<p><strong>Server Port:</strong> " . $_SERVER['SERVER_PORT'] . "</p>";
+
+echo "<h2>Server Time</h2>";
+echo "<p><strong>Current Time (Server):</strong> " . date('Y-m-d H:i:s') . "</p>";
+
+echo "<h2>PHP Environment</h2>";
+echo "<p><strong>PHP Version:</strong> " . phpversion() . "</p>";
+echo "<p><strong>PHP SAPI:</strong> " . php_sapi_name() . "</p>";
+
+phpinfo();
+?>
+</body>
+</html>
